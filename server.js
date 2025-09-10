60af9c69376ae288de4b229a51e60b86d1176256
Jean <jeannguilherme30@gmail.com>
2025-09-10 00:26:45 -0300

    Listar usuários sem expor senha

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import publicRoutes from './routes/public.js'
+import privateRoutes from './routes/private.js'
 
 const app = express()
 
 app.use(express.json())
 
 app.use('/', publicRoutes)
+app.use('/', privateRoutes)
 
 app.listen(3333, () => console.log("Servidor rodando!"))
